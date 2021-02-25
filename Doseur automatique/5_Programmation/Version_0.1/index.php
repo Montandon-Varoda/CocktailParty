237c08885fa6ceac07a60820c926d9966a1bb1f3
Montandon <montandonmaxime@gmail.com>
2021-02-25 16:19:15 +0100

    code

---
+++ Doseur automatique/5_Programmation/Version_0.1/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/">
+	<link href="img/">
+	<title>Cocktail Party</title>
+</head>
+<body>
+	<header>
+		<h1>Cocktail Party</h1>
+		<h2>Bienvenue sur le site des cocktail à gogo</h2>
+	</header>
+	<section>
+		<!--Cocktail-->
+		<div id="cocktail">
+			<img src="">
+			<div id="listeCocktail">
+				<a href="cocktail/listeCocktail.php">
+					<img src="">
+					<p>
+						
+					</p>
+				</a>
+			</div>
+			<div id="topCocktail">
+				<a href="cocktail/topCocktail.php">
+					<img src="">
+					<p>
+						
+					</p>
+				</a>
+			</div>
+		</div>
+		<!--Jeux-->
+		<div id="jeux">
+			<img src="">
+			<div id="roulette">
+				<a href="jeux/roulette.php">
+					<img src="">
+					<p>
+						
+					</p>
+				</a>
+			</div>
+			<div id="random">
+				<a href="">
+					<img src="">
+					<p>
+						
+					</p>
+				</a>
+			</div>
+		</div>
+		<!--Votre Cocktail-->
+		<div id="creation">
+			<a href="">
+				<img src="">
+				<p>
+					
+				</p>
+			</a>
+		</div>
+		<!--Présentation-->
+		<div id="presentation">
+			<a href="">
+				<img src="">
+				<p>
+					
+				</p>
+			</a>
+		</div>
+	</section>
+</body>
+</html>
